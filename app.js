b98fbe6b9795202fb1597641377a2aa4754d7158
Mel <meljohnpualon@gmail.com>
2023-07-23 15:01:32 +0800

    added mobile menu

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hey there!')
+const burgerMenu = document.querySelector('.icon-hamburger');
+const mobileMenu = document.querySelector('.menu-overlay');
+const closeMenu = document.querySelector('.close');
+
+
+burgerMenu.addEventListener('click', mobileShow);
+closeMenu.addEventListener('click', mobileHide);
+
+function mobileShow() {
+  mobileMenu.classList.add('open-menu')
+}
+
+function mobileHide() {
+  mobileMenu.classList.remove('open-menu')
+}
