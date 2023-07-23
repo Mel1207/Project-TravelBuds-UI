d31c89c0800b469d1f0fc5acd0c9066f9acc26ca
Mel <meljohnpualon@gmail.com>
2023-07-23 15:15:35 +0800

    fix mobile menu

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+// VARIABLES
 const burgerMenu = document.querySelector('.icon-hamburger');
 const mobileMenu = document.querySelector('.menu-overlay');
 const closeMenu = document.querySelector('.close');
+const navbar = document.querySelector('.navbar');
 
-
+// EVENTS
 burgerMenu.addEventListener('click', mobileShow);
 closeMenu.addEventListener('click', mobileHide);
+window.addEventListener('scroll', isScrolling);
 
+// FUNCTIONS
 function mobileShow() {
   mobileMenu.classList.add('open-menu')
 }
@@ -13,3 +17,16 @@ function mobileShow() {
 function mobileHide() {
   mobileMenu.classList.remove('open-menu')
 }
+
+function isScrolling() {
+  let scroll = window.scrollY 
+
+  if(scroll >= 50) {
+    navbar.classList.add('is-scroll')
+  } else {
+    navbar.classList.remove('is-scroll')
+  }
+
+  console.log(scroll)
+}
+
